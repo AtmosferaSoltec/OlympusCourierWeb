27b373bb354fb707bdbb1dda71d6bd2bcfaf500d
AppDevSYS <151794430+AtmosferaSoltec@users.noreply.github.com>
2023-11-22 20:56:34 -0500

    nuevo avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { RepartosComponent } from './components/repartos/repartos.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'menu', component: MenuComponent },
+    { path: 'repartos', component: RepartosComponent },
     { path: '**', redirectTo: 'login' }
 ];
---
+++ src/app/components/repartos/repartos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-repartos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './repartos.component.html',
+  styleUrl: './repartos.component.css'
+})
+export class RepartosComponent {
+
+}
