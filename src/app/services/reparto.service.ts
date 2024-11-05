3a662f7d29100a3e439136b8c8de460dda4df46a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2024-11-05 10:41:39 -0500

    Mandar token

--- src/app/services/reparto.service.ts
+++ src/app/services/reparto.service.ts
@@ -4,39 +4,38 @@ import { environment } from '../../environments/environment.development';
 import { Result } from '../interfaces/state';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RepartoService {
-
   http = inject(HttpClient);
   url = `${environment.baseUrl}/api/repartos`;
   newUrl = `${environment.newUrl}/api/reparto`;
-  
 
   get(id: number) {
     return this.http.get<Result>(`${this.url}/${id}`);
   }
 
   getAll(params: any) {
-    return this.http.get<Result>(this.url, { params: params })
+    return this.http.get<Result>(this.url, { params: params });
   }
 
-  getAllNew(
-    params: any
-  ) {
-    return this.http.get(this.newUrl, { params: params });
+  getAllNew(params: any) {
+    const query = this.http.get(this.newUrl, { params: params });
+    return query
+    
   }
 
   setActivo(id_reparto: number, activo: 'S' | 'N') {
-    return this.http.patch<Result>(`${this.url}/activo/${id_reparto}`, { activo });
+    return this.http.patch<Result>(`${this.url}/activo/${id_reparto}`, {
+      activo,
+    });
   }
 
   insert(body: any) {
-    return this.http.post<Result>(this.url, body)
+    return this.http.post<Result>(this.url, body);
   }
 
   update(id: number, body: any) {
-    return this.http.patch<Result>(`${this.url}/${id}`, body)
+    return this.http.patch<Result>(`${this.url}/${id}`, body);
   }
-  
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     //baseUrl: 'http://localhost:7878',
     baseUrl: 'https://sv-YaAUgKfBpu.cloud.elastika.pe:7878',
-    //newUrl: 'http://localhost:3000',
+    //newUrl: 'http://localhost:7979',
     newUrl: 'https://sv-YaAUgKfBpu.cloud.elastika.pe:7979',
     token: ''
 };
