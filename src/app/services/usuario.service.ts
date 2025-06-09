2313c3c4456f34f3440b1f54513fd8ee8d96c635
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-06-09 01:10:38 -0500

    Actualizar URLs en el entorno de desarrollo y mejorar la estructura del servicio de usuario

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,58 +1,55 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
-import { environment } from '../../environments/environment.development';
 import { Usuario } from '../interfaces/usuario';
 import { Result } from '../interfaces/state';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
-
-
   http = inject(HttpClient);
   baseUrl = `${environment.baseUrl}/api/usuarios`;
 
-  usuario = signal<Usuario | undefined>(undefined)
+  usuario = signal<Usuario | undefined>(undefined);
 
   validarSesion() {
-    this.http.get<Result>(`${this.baseUrl}/verificarToken`)
-      .subscribe({
-        next: (res: any) => {
-          if (res?.isSuccess) {
-            this.usuario.set(res.data);
-          } else {
-            console.log(res?.mensaje);
-          }
-        },
-        error: (err: any) => {
-          alert(err.message)
-          console.log(err);
+    this.http.get<Result>(`${this.baseUrl}/verificarToken`).subscribe({
+      next: (res: any) => {
+        if (res?.isSuccess) {
+          this.usuario.set(res.data);
+        } else {
+          console.log(res?.mensaje);
         }
-      })
+      },
+      error: (err: any) => {
+        alert(err.message);
+        console.log(err);
+      },
+    });
   }
 
   /** Funciones de Login **/
   login(doc: string, clave: string) {
     return this.http.post<Result>(`${this.baseUrl}/login`, {
       documento: doc,
-      clave: clave
-    })
+      clave: clave,
+    });
   }
 
   async isLogged(): Promise<boolean> {
-    const call = this.http.get(`${this.baseUrl}/verificarToken`)
+    const call = this.http.get(`${this.baseUrl}/verificarToken`);
     const res: any = await firstValueFrom(call);
     return res?.isSuccess;
   }
 
   getAll(params: any) {
-    return this.http.get<Result>(this.baseUrl, { params: params })
+    return this.http.get<Result>(this.baseUrl, { params: params });
   }
 
   get(id: number) {
-    return this.http.get<Result>(`${this.baseUrl}/${id}`)
+    return this.http.get<Result>(`${this.baseUrl}/${id}`);
   }
 
   add(form: any) {
@@ -66,12 +63,15 @@ export class UsuarioService {
   delete(id: number | undefined, estado: string) {
     const url = `${this.baseUrl}/${id}`;
     const body = {
-      activo: estado
-    }
+      activo: estado,
+    };
     return this.http.patch<Result>(url, body);
   }
 
   cambiarPass(pass_anterior: string, pass_nueva: string) {
-    return this.http.post<Result>(`${this.baseUrl}/cambiarPass`, { pass_anterior, pass_nueva })
+    return this.http.post<Result>(`${this.baseUrl}/cambiarPass`, {
+      pass_anterior,
+      pass_nueva,
+    });
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     //baseUrl: 'http://localhost:7878',
-    baseUrl: 'https://atmosfera-soltec.com:7878',
-    newUrl: 'https://atmosfera-soltec.com:7979',
+    baseUrl: 'https://atmosfera-soltec.online:7878',
+    newUrl: 'https://atmosfera-soltec.online:7979',
     //newUrl: 'https://atmosfera-soltec.com:7979',
     token: ''
 };
