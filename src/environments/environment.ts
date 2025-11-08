2052af5fbfc712b5ab38e631d5f9b951c4671fa9
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-11-08 15:36:13 -0500

    Refactor la tabla de repartos para mejorar la presentación y la carga de datos, y actualizar las URLs en el entorno de desarrollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseUrl: 'https://atmosfera-soltec.online:7878',
-    newUrl: 'https://atmosfera-soltec.online:7979',
+    baseUrl: 'https://atmosfera-soltec.online/olympus-courier-express',
+    newUrl: 'https://atmosfera-soltec.online/olympus-courier-nestjs',
     token: ''
 };
