7a68fcf2d7a3c6f997dc7d5b92feaba944f9151d
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-04-07 18:06:47 -0500

    local environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  //baseUrl: 'http://localhost:7878',
-  //newUrl: 'http://localhost:7979',
-  baseUrl: 'https://atmosfera-soltec.com/olympus-courier-express',
-  newUrl: 'https://atmosfera-soltec.com/olympus-courier-nestjs',
+  baseUrl: 'http://localhost:7878',
+  newUrl: 'http://localhost:7979',
+  //baseUrl: 'https://atmosfera-soltec.com/olympus-courier-express',
+  //newUrl: 'https://atmosfera-soltec.com/olympus-courier-nestjs',
   token: '',
 };
